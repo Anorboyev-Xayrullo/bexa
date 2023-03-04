81280d2b840c193cbaf43f50edb4dc9a8a2dc9c6
Xayrullo Anorboyev <anorboyevxayrullo841@gmail.com>
2023-03-04 15:38:19 +0500

    fix

---
+++ node.js
@@ -0,0 +1,25 @@
+let a = +prompt("birinchi sonni kiriting")
+let b = +prompt("ikkinchi sonni kiriting")
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(a - b);
+
+
+
+function num(a,b){
+    return  a + b 
+}
+console.log(num(3,5));
+
+
+let name = "Aziza"
+
+let age = 19
+
+console.log("Mening ismim "+ name +" yoshim "+ age +" da");
+
+
+
+
